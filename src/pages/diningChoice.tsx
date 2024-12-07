7be9d2133014525c88196b28590e46f12206f6c9
yusuShin <tlsdbtn0507@naver.com>
2024-12-07 21:40:54 +0900

    feat : 포장여부 수정 - 오더 폼 로직에 따라 로컬스토리지에 저장할 정보 수정

--- src/pages/diningChoice.tsx
+++ src/pages/diningChoice.tsx
@@ -1,27 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import { setOrderInfo } from "../feat/order";
 
-type DiningChoice = "dine_in" | "takeaway";
 
 const DiningChoice: React.FC = () => {
   const naviate = useNavigate();
 
-  const confirmDiningType = (dineType: DiningChoice) => {
-    localStorage.setItem("diningChoice", dineType);
+  const confirmDiningType = (dineType: boolean) => {
+    setOrderInfo("isTakeOut", dineType)
     naviate("/menus");
   };
-
-  useEffect(() => {
-    localStorage.removeItem("diningChoice");
-  }, []);
 
   return (
     <>
       <h1>
         식사 장소를 <br /> 선택해주세요
       </h1>
-      <button onClick={() => confirmDiningType("dine_in")}>매장 식사</button>
-      <button onClick={() => confirmDiningType("takeaway")}>포장</button>
+      <button onClick={() => confirmDiningType(false)}>매장 식사</button>
+      <button onClick={() => confirmDiningType(true)}>포장</button>
     </>
   );
 };
