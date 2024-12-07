813ba57565702966ace83161ddd801b88dc6f393
kkoomin <kkooming2@gmail.com>
2024-12-07 09:49:33 +0000

    feat: taste 카테고리 추가

---
+++ src/types/category.interface.ts
@@ -0,0 +1,11 @@
+export interface MenuCategory {
+  id: string;
+  name: string;
+  description: string;
+  image_url: string;
+}
+
+export interface Taste {
+  id: string;
+  name: string;
+}
--- src/types/menu.interface.ts
+++ src/types/menu.interface.ts
@@ -1,16 +1,10 @@
-export interface MenuCategory {
-  id: string;
-  name: string;
-  description: string;
-  image_url: string;
-}
-
 export interface MenuItem {
   id: string;
   name: string;
   description: string;
   price: number;
   category_id: string;
+  taste_ids: string[];
   image_url: string;
 }
 
