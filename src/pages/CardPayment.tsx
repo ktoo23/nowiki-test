17df947910da00997dec0e171e837be1de226ada
kkoomin <kkooming2@gmail.com>
2024-12-07 13:29:03 +0000

    feat: tailwind 테스트

--- src/pages/CardPayment.tsx
+++ src/pages/CardPayment.tsx
@@ -9,13 +9,13 @@ const CardPayment = () => {
   };
 
   return (
-    <>
+    <section className="flex flex-col justify-center">
       <h1>결제를 진행해주세요</h1>
-      <div className="card-paymenet_image">
+      <div className="w-[500px] m-auto">
         <img src={CardPaymentImage} alt="카드 결제 안내 이미지" />
       </div>
       <button onClick={navigateToCardPayment}>취소</button>
-    </>
+    </section>
   );
 };
 
