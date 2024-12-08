fbf733fd08e3ea6c3ee7891624b289c432c7c07c
kkoomin <kkooming2@gmail.com>
2024-12-08 07:10:55 +0000

    feat: 버튼 스타일 적용

--- src/pages/CardPayment.tsx
+++ src/pages/CardPayment.tsx
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import CardPaymentImage from "../assets/image/card-payment.png";
+import { Button } from "@/components/ui/button";
 
 const CardPayment = () => {
   const naviate = useNavigate();
 
-  const navigateToCardPayment = () => {
+  const navigateToPaymentSelect = () => {
     naviate("/payment-select");
   };
 
@@ -14,7 +15,13 @@ const CardPayment = () => {
       <div className="w-[500px] m-auto">
         <img src={CardPaymentImage} alt="카드 결제 안내 이미지" />
       </div>
-      <button onClick={navigateToCardPayment}>취소</button>
+      <Button
+        variant="outline"
+        className="w-[100px] h-[50px]"
+        onClick={navigateToPaymentSelect}
+      >
+        취소
+      </Button>
     </section>
   );
 };
--- src/pages/PaymentSelect.tsx
+++ src/pages/PaymentSelect.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 const PaymentSelect = () => {
   const naviate = useNavigate();
@@ -10,8 +11,18 @@ const PaymentSelect = () => {
   return (
     <>
       <h1>결제 방법을 선택해주세요</h1>
-      <button>모바일 상품권</button>
-      <button onClick={navigateToCardPayment}>카드 결제</button>
+      <div className="flex gap-[50px] justify-center items-center">
+        <Button variant="outline" className="w-[100px] h-[100px]">
+          모바일 상품권
+        </Button>
+        <Button
+          variant="outline"
+          className="w-[100px] h-[100px]"
+          onClick={navigateToCardPayment}
+        >
+          카드 결제
+        </Button>
+      </div>
     </>
   );
 };
