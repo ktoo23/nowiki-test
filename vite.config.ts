73745c39c7576ec3ca72d254ba68416dba24082f
jong6598 <jong6598@naver.com>
2024-12-09 01:39:50 +0900

    remove: 에러 및 충돌을 발생시키는 vite config 설정 초기화

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-    plugins: [
-        react(),
-        legacy({
-            targets: ['defaults', 'not IE 11'],
-            additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        }),
-    ],
+    plugins: [react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
