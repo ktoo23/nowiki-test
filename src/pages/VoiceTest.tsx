d51604cbc80e24b6293189ceb2328552531d6147
jong6598 <jong6598@naver.com>
2024-12-08 14:21:51 +0900

    add: 음성인식 기능 테스트를 위한 임시 페이지 추가

---
+++ src/pages/VoiceTest.tsx
@@ -0,0 +1,11 @@
+import Voice from '../components/voice/voice'
+
+const VoiceTest: React.FC = () => {
+    return (
+        <>
+            <Voice />
+        </>
+    )
+}
+
+export default VoiceTest
