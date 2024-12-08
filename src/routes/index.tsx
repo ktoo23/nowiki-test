ad71831e186e99c9fa73b616596432fa8a878e25
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-08 21:34:18 +0900

    fix : menu-select -> menu-select/:id

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,61 +1,61 @@
-import { createBrowserRouter } from 'react-router-dom'
-import MenuSelect from '../pages/MenuSelect'
-import MenuList from '@/pages/MenuList'
-import SetSelection from '@/pages/SetSelection'
-import OrderHistory from '../pages/OrderHistory'
-import PointCollection from '../pages/PointCollection'
-import FoodOffer from '../pages/FoodOffer'
-import CardPayment from '../pages/CardPayment'
-import PaymentSelect from '../pages/PaymentSelect'
-import MainPage from '@/pages/MainPage'
-import DiningSelection from '@/pages/DiningSelection'
-import VoiceTest from '@/pages/VoiceTest'
+import { createBrowserRouter } from "react-router-dom";
+import MenuSelect from "../pages/MenuSelect";
+import MenuList from "@/pages/MenuList";
+import SetSelection from "@/pages/SetSelection";
+import OrderHistory from "../pages/OrderHistory";
+import PointCollection from "../pages/PointCollection";
+import FoodOffer from "../pages/FoodOffer";
+import CardPayment from "../pages/CardPayment";
+import PaymentSelect from "../pages/PaymentSelect";
+import MainPage from "@/pages/MainPage";
+import DiningSelection from "@/pages/DiningSelection";
+import VoiceTest from "@/pages/VoiceTest";
 
 const routes = createBrowserRouter([
     {
-        path: '/',
-        element: <MainPage />,
+        path: "/",
+        element: <MainPage />
     },
     {
-        path: '/menus',
-        element: <MenuList />,
+        path: "/menus",
+        element: <MenuList />
     },
     {
-        path: '/set-choice/:itemId',
+        path: "/set-choice/:itemId",
         element: <SetSelection />
     },
     {
-        path: '/menu-select',
-        element: <MenuSelect />,
+        path: "/menu-select/:id",
+        element: <MenuSelect />
     },
     {
-        path: 'dining-choice',
-        element: <DiningSelection />,
+        path: "dining-choice",
+        element: <DiningSelection />
     },
     {
-        path: '/order-history',
-        element: <OrderHistory />,
+        path: "/order-history",
+        element: <OrderHistory />
     },
     {
-        path: '/point-collection',
-        element: <PointCollection />,
+        path: "/point-collection",
+        element: <PointCollection />
     },
     {
-        path: '/food-offer',
-        element: <FoodOffer />,
+        path: "/food-offer",
+        element: <FoodOffer />
     },
     {
-        path: '/payment-select',
-        element: <PaymentSelect />,
+        path: "/payment-select",
+        element: <PaymentSelect />
     },
     {
-        path: '/card-payment',
-        element: <CardPayment />,
+        path: "/card-payment",
+        element: <CardPayment />
     },
     {
-        path: '/voice-test',
-        element: <VoiceTest />,
-    },
-])
+        path: "/voice-test",
+        element: <VoiceTest />
+    }
+]);
 
-export default routes
+export default routes;
