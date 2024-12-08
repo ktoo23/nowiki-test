bb3b88cc7fbeae464e60d48fe0fbec62ebf3c28a
jong6598 <jong6598@naver.com>
2024-12-09 00:04:42 +0900

    hotfix: 배포 에러해결을 위한 babel 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
-import babel from 'vite-plugin-babel'
 
 export default defineConfig({
     plugins: [
@@ -10,13 +9,6 @@ export default defineConfig({
         legacy({
             targets: ['defaults', 'not IE 11'],
             additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        }),
-        babel({
-            babelConfig: {
-                babelrc: false,
-                configFile: false,
-                plugins: ['@babel/plugin-transform-runtime'],
-            },
         }),
     ],
     resolve: {
