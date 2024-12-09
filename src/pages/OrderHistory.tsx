282ccba765559b87fc96bb2ac8ca25ae00b72b94
kkoomin <kkooming2@gmail.com>
2024-12-09 10:02:07 +0000

    fix: 미사용 import 삭제

--- src/pages/OrderHistory.tsx
+++ src/pages/OrderHistory.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import {
   Tooltip,
