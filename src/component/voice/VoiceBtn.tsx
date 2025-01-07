893c1cd3401730125689555af06805fec7e657b5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-07 16:18:11 +0900

    fix : 인라인 스타일 수정, variant outline 추가

--- src/component/voice/VoiceBtn.tsx
+++ src/component/voice/VoiceBtn.tsx
@@ -1,18 +1,20 @@
-import { Button } from "@/components/ui/button"
-import { useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const VoiceBtn = () => {
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    return (
+        <div className="flex items-center">
+            <Button
+                variant="outline"
+                className="bg-mc_yellow text-black"
+                onClick={() => navigate("/voice-test")}
+            >
+                <p>음성으로 주문하기</p>
+            </Button>
+        </div>
+    );
+};
 
-
-  return (
-    <div className="flex items-center">
-      <Button className="bg-mc_yellow text-black" onClick={() => navigate('/voice-test')}>
-        <p>음성으로 주문하기</p>
-      </Button>
-    </div>
-  )
-}
-
-export default VoiceBtn
+export default VoiceBtn;
