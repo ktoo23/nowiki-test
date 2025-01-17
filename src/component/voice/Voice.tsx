964dedbcdfbb115f8c8acfc42895fc0fe5fd45d2
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-17 22:14:21 +0900

    fix : 검색 결과의 발음에 따른 조사 구분

--- src/component/voice/Voice.tsx
+++ src/component/voice/Voice.tsx
@@ -1,196 +1,165 @@
-import { useState, useEffect, useCallback, useMemo } from 'react'
-import useSpeechToText from '../../hooks/useSpeechToText'
-import MicImage from '../../assets/image/mic.png'
-import Fuse from 'fuse.js'
-import menuData from '../../assets/data/menu.json'
-import { MenuItem } from '@/types/menu.interface'
-import VoiceSearchedMenuList from './VoiceSearchedMenuList'
-import { Button } from '@/components/ui/button'
-import { useNavigate } from 'react-router-dom'
-import { setItemToOrderInfo } from '@/feat/order'
+import { useState, useEffect, useCallback, useMemo } from "react";
+import useSpeechToText from "../../hooks/useSpeechToText";
+import MicImage from "../../assets/image/mic.png";
+import Fuse from "fuse.js";
+import menuData from "../../assets/data/menu.json";
+import { MenuItem } from "@/types/menu.interface";
+import VoiceSearchedMenuList from "./VoiceSearchedMenuList";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { setItemToOrderInfo } from "@/feat/order";
+import { josa } from "es-hangul";
 
 interface FuzeItem {
-    item: MenuItem
-    refIndex?: number
+    item: MenuItem;
+    refIndex?: number;
 }
 
 const recommendedMenu = [
     {
         item: menuData.menu_items[0],
-        refIndex: 0,
+        refIndex: 0
     },
     {
         item: menuData.menu_items[4],
-        refIndex: 4,
+        refIndex: 4
     },
     {
         item: menuData.menu_items[7],
-        refIndex: 7,
+        refIndex: 7
     },
     {
         item: menuData.menu_items[8],
-        refIndex: 8,
-    },
-]
+        refIndex: 8
+    }
+];
 
 const Voice = () => {
-    const { transcript, listening, toggleListening } = useSpeechToText()
-    const [searchResults, setSearchResults] = useState<FuzeItem[]>([])
-    const [selectedMenuItems, setSelectedMenuItems] = useState<MenuItem[]>([])
-    const navigate = useNavigate()
-    const [searchComplete, setSearchComplete] = useState<boolean>(false)
+    const { transcript, listening, toggleListening } = useSpeechToText();
+    const [searchResults, setSearchResults] = useState<FuzeItem[]>([]);
+    const [selectedMenuItems, setSelectedMenuItems] = useState<MenuItem[]>([]);
+    const navigate = useNavigate();
+    const [searchComplete, setSearchComplete] = useState<boolean>(false);
 
     const onClickVoiceButton = () => {
-        toggleListening()
-        setSearchResults([])
-        setSelectedMenuItems([])
-        setSearchComplete(false)
-    }
+        toggleListening();
+        setSearchResults([]);
+        setSelectedMenuItems([]);
+        setSearchComplete(false);
+    };
 
     const menuFuse = useMemo(
         () =>
             new Fuse(menuData.menu_items, {
-                keys: ['name', 'description'],
+                keys: ["name", "description"]
             }),
-        [],
-    )
+        []
+    );
 
     useEffect(() => {
         if (transcript && !listening && !searchComplete) {
-            const menuItemResults = menuFuse.search(transcript)
-            setSearchComplete(true)
-            setSearchResults(menuItemResults)
+            const menuItemResults = menuFuse.search(transcript);
+            setSearchComplete(true);
+            setSearchResults(menuItemResults);
         }
-    }, [transcript, listening, searchComplete, menuFuse])
+    }, [transcript, listening, searchComplete, menuFuse]);
 
     const handleMenuItemClick = useCallback((item: MenuItem) => {
         setSelectedMenuItems((prevItems) => {
-            const isItemSelected = prevItems.some((selectedItem) => selectedItem.id === item.id)
+            const isItemSelected = prevItems.some(
+                (selectedItem) => selectedItem.id === item.id
+            );
             if (isItemSelected) {
-                return prevItems.filter((selectedItem) => selectedItem.id !== item.id)
+                return prevItems.filter(
+                    (selectedItem) => selectedItem.id !== item.id
+                );
             } else {
-                return [...prevItems, item]
+                return [...prevItems, item];
             }
-        })
-    }, [])
+        });
+    }, []);
 
     const handleAddShoppingCart = () => {
-        selectedMenuItems.forEach((item) => setItemToOrderInfo(item))
+        selectedMenuItems.forEach((item) => setItemToOrderInfo(item));
 
-        alert('장바구니에 추가되었습니다.')
-    }
+        alert("장바구니에 추가되었습니다.");
+    };
 
     const handleQuickOrder = () => {
-        selectedMenuItems.forEach((item) => setItemToOrderInfo(item))
-        navigate('/order-history')
-    }
+        selectedMenuItems.forEach((item) => setItemToOrderInfo(item));
+        navigate("/order-history");
+    };
+
+    const getSeachText = (str: string) => {
+        return josa(str, "으로/로");
+    };
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                padding: '2rem',
-                maxWidth: '600px',
-                margin: '0 auto',
-                paddingBottom: '160px',
-                position: 'relative',
-            }}
-        >
-            <h1 style={{ marginBottom: '1rem', fontWeight: '600' }}>말해서 메뉴 찾기</h1>
-            <p style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <div className="flex flex-col items-center p-8 max-w-[600px] mx-auto my-0 pb-40 relative">
+            <h1 className="mb-4 font-semibold">말해서 메뉴 찾기</h1>
+            <p className="text-center mb-8">
                 마이크 버튼을 클릭하고 검색하려는 메뉴의 이름을 말해주세요.
             </p>
             <button
                 onClick={onClickVoiceButton}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100px',
-                    height: '100px',
-                    border: 'none',
-                    borderRadius: '50%',
-                    backgroundColor: listening ? '#4CAF50' : '#f0f0f0',
-                    boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-                    transition: 'all 0.3s ease',
-                    cursor: 'pointer',
-                }}
+                className={`flex justify-center items-center w-[100px] h-[100px] border-none rounded-[50%] ${
+                    listening ? "bg-[#4caf50]" : "bg-[#f0f0f0]"
+                } cursor-pointer`}
             >
                 <img
                     src={MicImage}
-                    alt='마이크 이미지'
+                    alt="마이크 이미지"
                     style={{
-                        height: '50px',
-                        filter: listening ? 'brightness(0) invert(1)' : 'none',
+                        height: "50px",
+                        filter: listening ? "brightness(0) invert(1)" : "none"
                     }}
                 />
             </button>
             <p
-                style={{
-                    marginTop: '1rem',
-                    fontWeight: 'bold',
-                    color: listening ? '#4CAF50' : '#333',
-                }}
+                className={`mt-4 font-bold ${
+                    listening ? "bg-[#4caf50]" : "bg-[#333]"
+                }`}
             >
-                {listening ? '음성인식 중...' : '음성인식 시작'}
+                {listening ? "음성인식 중..." : "음성인식 시작"}
             </p>
             {transcript && !listening && (
                 <div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            marginTop: '1rem',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <p style={{ fontWeight: '700', fontSize: '1.2rem' }}> {transcript}</p>
-                        <p>로 검색중이예요</p>
-                        <div className='flex space-x-1 ml-2'>
-                            <span className='animate-pulse w-1 h-1 bg-gray-500 rounded-full inline-block'></span>
+                    <div className="flex mt-4 justify-center items-center">
+                        <p>{getSeachText(transcript)}</p>
+                        <div className="flex space-x-1 ml-2">
+                            <span className="animate-pulse w-1 h-1 bg-gray-500 rounded-full inline-block"></span>
                             <span
-                                className='animate-pulse w-1 h-1 bg-gray-500 rounded-full inline-block'
-                                style={{ animationDelay: '200ms' }}
+                                className="animate-pulse w-1 h-1 bg-gray-500 rounded-full inline-block"
+                                style={{ animationDelay: "200ms" }}
                             ></span>
                             <span
-                                className='animate-pulse w-1 h-1 bg-gray-500 rounded-full inline-block'
-                                style={{ animationDelay: '400ms' }}
+                                className="animate-pulse w-1 h-1 bg-gray-500 rounded-full inline-block"
+                                style={{ animationDelay: "400ms" }}
                             ></span>
                         </div>
                     </div>
                     {searchResults.length > 0 ? (
-                        <div style={{ width: '100%', marginTop: '2rem' }}>
-                            <h2
-                                style={{
-                                    textAlign: 'center',
-                                    marginBottom: '1rem',
-                                    fontWeight: 'bold',
-                                }}
-                            >
+                        <div style={{ width: "100%", marginTop: "2rem" }}>
+                            <h2 className="text-center mb-4 font-bold">
                                 다음과 같은 메뉴들을 발견했어요!
                             </h2>
                             <VoiceSearchedMenuList
-                                items={searchResults.map((result) => result.item)}
+                                items={searchResults.map(
+                                    (result) => result.item
+                                )}
                                 onClick={handleMenuItemClick}
                                 selectedItems={selectedMenuItems}
                             />
                         </div>
                     ) : (
                         <div>
-                            <p
-                                style={{
-                                    marginTop: '2rem',
-                                    textAlign: 'center',
-                                    fontWeight: 'bold',
-                                }}
-                            >
-                                검색 결과가 없습니다. 이런 메뉴는 어때요??
+                            <p className="mt-8 text-center font-bold">
+                                검색 결과가 없습니다. 이런 메뉴는 어때요?
                             </p>
                             <VoiceSearchedMenuList
-                                items={recommendedMenu.map((result) => result.item)}
+                                items={recommendedMenu.map(
+                                    (result) => result.item
+                                )}
                                 onClick={handleMenuItemClick}
                                 selectedItems={selectedMenuItems}
                             />
@@ -199,26 +168,26 @@ const Voice = () => {
                 </div>
             )}
             {/** 아래부터 새로 추가 */}
-            <div className='max-w-screen-sm fixed bottom-0 left-2/1 p-4 flex w-full h-[150px] justify-around bg-white'>
-                <div className='mr-3 flex flex-col w-[280px]'>
+            <div className="max-w-screen-sm fixed bottom-0 left-2/1 p-4 flex w-full h-[150px] justify-around bg-white">
+                <div className="mr-3 flex flex-col w-[280px]">
                     <Button
-                        variant='secondary'
-                        className='mb-3 h-[70px] text-lg'
+                        variant="secondary"
+                        className="mb-3 h-[70px] text-lg"
                         onClick={handleAddShoppingCart}
                     >
                         장바구니에 넣기
                     </Button>
                     <Button
-                        variant='secondary'
-                        className='h-[70px] text-lg'
-                        onClick={() => navigate('/menus')}
+                        variant="secondary"
+                        className="h-[70px] text-lg"
+                        onClick={() => navigate("/menus")}
                     >
                         처음으로
                     </Button>
                 </div>
-                <div className='grow'>
+                <div className="grow">
                     <Button
-                        className='w-full h-full bg-mc_yellow hover:bg-mc_yellow text-lg'
+                        className="w-full h-full bg-mc_yellow hover:bg-mc_yellow text-lg"
                         onClick={handleQuickOrder}
                     >
                         바로 결제하기
@@ -226,7 +195,7 @@ const Voice = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Voice
+export default Voice;
