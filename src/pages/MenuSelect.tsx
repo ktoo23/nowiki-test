468829bd5e02f22534716c2d5d6bcd52f89bf634
yusuShin <tlsdbtn0507@naver.com>
2025-02-01 21:28:21 +0900

    chore : component 폴더 제거 및 정리

--- src/pages/MenuSelect.tsx
+++ src/pages/MenuSelect.tsx
@@ -1,7 +1,7 @@
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import AddShoppingCartButton from "@/component/AddShoppingCartButton";
-import QuickPayButton from "@/component/QuickPayButton";
-import CancelButton from "@/component/CancelButton";
+import AddShoppingCartButton from "@/components/AddShoppingCartButton";
+import QuickPayButton from "@/components/QuickPayButton";
+import CancelButton from "@/components/CancelButton";
 import { menu_items } from "../assets/data/menu.json";
 
 import { getOrderInfo, setItemToOrderInfo } from "@/feat/order";
